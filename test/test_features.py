1416ea98f8aabd24f10155a1fca41ccbae084284
Aaron Virshup <avirshup@gmail.com>
2017-09-26 17:47:05 -0700

    Test that paths are resolved correctly

--- test/test_features.py
+++ test/test_features.py
@@ -4,3 +4,6 @@ import subprocess
 def test_multiple_bases():
         subprocess.check_call(['docker-make', '-f', 'data/multibase.yml', 'target2', 'target3'])
 
+
+def test_paths_relative_interpreted_relative_to_definition_file():
+        subprocess.check_call(['docker-make', '-f', 'data/include.yml', 'target'])
