2f660f71f9d35c3430fbd81377f958d81969304e
Aaron Virshup <avirshup@gmail.com>
2017-10-02 14:28:43 -0700

    Test file creation and properly clean up environments

---
+++ test/helpers.py
@@ -0,0 +1,56 @@
+import os
+import io
+import tarfile
+import pytest
+import docker.errors
+
+__client = None
+
+def _get_client():
+    """
+    Returns:
+        docker.APIClient
+    """
+    global __client
+    if __client is None:
+        __client = docker.from_env()
+    return __client
+
+
+def creates_images(*imgnames):
+    """ Creates fixtures to make sure to remove named images after (and before, if necessary)
+    running a test
+    """
+    @pytest.fixture
+    def fixture():
+        client = _get_client()
+        for name in imgnames:
+            try:
+                client.images.remove(name, force=True)
+            except docker.errors.ImageNotFound:
+                pass
+
+        yield
+
+        for name in imgnames:  # force it to also remove the containers
+            client.images.remove(name, force=True)
+    return fixture
+
+
+def assert_file_content(imgname, path, content):
+    """ Asserts that an image exists with a file at the
+    specified path containing the specified content
+    """
+    client = _get_client()
+    try:
+        image = client.images.get(imgname)
+    except (docker.errors.ImageNotFound, docker.errors.APIError) as exc:
+        assert False, "Image %s not found: %s" % (imgname, exc)
+
+    container = client.containers.create(image)
+    tarstream, stat = container.get_archive(path)
+    container.remove()
+
+    tf = tarfile.open(fileobj=io.BytesIO(tarstream.read()))
+    val = tf.extractfile(os.path.basename(path)).read().decode('utf-8')
+    assert val.strip() == content
--- test/test_features.py
+++ test/test_features.py
@@ -1,14 +1,29 @@
-import subprocess
+import pytest
+from dockermake.__main__ import _runargs as run_docker_make
+
+from .helpers import assert_file_content, creates_images
 
 
-def test_multiple_bases():
-    subprocess.check_call(['docker-make', '-f', 'data/multibase.yml', 'target2', 'target3'])
+# note: these tests MUST be run with CWD REPO_ROOT/tests
+
+img1 = creates_images(*'target2_bases target3_bases'.split())
+def test_multiple_bases(img1):
+    run_docker_make('-f data/multibase.yml target2_bases target3_bases')
+    assert_file_content('target2_bases', '/opt/success', 'success2')
+    assert_file_content('target3_bases', '/opt/success', 'success3')
 
 
-def test_paths_relative_interpreted_relative_to_definition_file():
-    subprocess.check_call(['docker-make', '-f', 'data/include.yml', 'target'])
+img2 = creates_images('target_include')
+def test_paths_relative_interpreted_relative_to_definition_file(img2):
+    run_docker_make('-f data/include.yml target_include')
+    assert_file_content('target_include', '/opt/testfile.txt',
+                        'this is a file used in tests for relative path resolution')
 
 
-def test_ignore_string():
-    subprocess.check_call(['docker-make', '-f', 'data/ignores.yml', 'target'])
+img3 = creates_images('target_ignore_string')
+def test_ignore_string(img3):
+    run_docker_make('-f data/ignores.yml target_ignore_string')
+    assert_file_content('target_ignore_string', '/opt/a', 'a')
+    assert_file_content('target_ignore_string', '/opt/c', 'c')
+
 
